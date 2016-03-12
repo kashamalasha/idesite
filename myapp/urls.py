b02979edae17f6b516be657bd4451f13a4c47f7a
kashamalasha <dmitry.burnyshev@gmail.com>
2016-03-12 09:50:43 +0300

    url, views

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /myapp/
     url(r'^$', views.index, name='index'),
+    # ex: /myapp/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /myapp/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /myapp/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
